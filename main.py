1fa100e8045aed38848fd53c1003e6cc47cba23a
John Perez <55162758+jjperez9000@users.noreply.github.com>
2022-03-30 11:43:10 -0400

    a bunch of updates

--- main.py
+++ main.py
@@ -1,28 +1,59 @@
 # bot.py
 import json
+import random
+import emoji
 import discord
 from discord.utils import get
 from datetime import datetime
 import threading
 import pytz
+from collections import OrderedDict
+import calendar
+from datetime import date
 
 intents = discord.Intents().all()
 client = discord.Client(prefix='', intents=intents)
+john = 261236642680012802
 
 with open('secrets/cfg.json', 'r') as f:
     data = json.load(f)
     currentGuild = data["guild"]
     responseChannel = data["channel"]
 
+destinationChannel = None
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+    global destinationChannel
+    destinationChannel = client.get_channel(responseChannel)
+
+
 @client.event
 async def on_message(message):
+    global destinationChannel
+
     print(message.author.id)
+    channel = client.get_channel(responseChannel)
+
+    # code for wordlebot to talk
+    if type(message.channel) == discord.channel.DMChannel:
+        print("DM : " + message.author.name + " : " + message.content)
+    else:
+        print(message.channel.name + " : " + message.author.name + " : " + message.content)
+    # if john sends a message, repeat it in channel
+    if message.author != client.user and type(message.channel) == discord.channel.DMChannel and message.author.id == john:
+        if ('!changechan' in message.content):
+            command = message.content.split(' ')
+            destinationChannel = client.get_channel(int(command[1]))
+        else:
+            await destinationChannel.send(message.content)
+
+    # make sure message is valid
     if message.author == client.user or message.channel.id != responseChannel:
         return
 
     # this will break after day 999
     # handle wordle submissions
-    channel = client.get_channel(responseChannel)
     if message.content[:7] == '!Wordle':
         await handle_submission(message)
     elif message.content[:12] == '!leaderboard':
@@ -37,7 +68,7 @@ async def on_message(message):
     elif message.content == '!test':
         print("working")
         await channel.send("test!")
-    elif message.content == '!reset' and message.author.id == 261236642680012802:
+    elif message.content == '!reset' and message.author.id == john:
         await channel.send("resetting the day, captain")
         await force_reset()
     elif message.content == '!reset':
@@ -62,8 +93,8 @@ async def print_stats(message):
         data = json.load(f)
         score = data[str(message.author.id)]["total_score"]
         completions = data[str(message.author.id)]["completions"]
-
-    await channel.send("Stats for **" + str(user) + "**. Completed " + str(completions) + ". Total Score: " + str(score))
+    averageScore = score/completions
+    await channel.send("Stats for **" + str(user) + "**. Completed " + str(completions) + ". Total Score: " + str(score) + " Average Score: " + str(averageScore)[:4])
 
 
 async def print_help(message):
@@ -100,7 +131,7 @@ async def print_leaderboard(message, key):
 
         players = []
         for player in data:
-            players.append({'id':player, key:data[player][key]})
+            players.append({'id': player, key: data[player][key]})
 
         if key == "weekly_score":
             players.sort(key=sortWeekly, reverse=True)
@@ -110,11 +141,11 @@ async def print_leaderboard(message, key):
         i = 1
         leaderboard = "```"
         leaderboard += '{message:{fill}{align}{width}}'.format(
-               message="   standings:",
-               fill=' ',
-               align='<',
-               width=15,
-            ) + " | score | completions\n"
+            message="   standings:",
+            fill=' ',
+            align='<',
+            width=15,
+        ) + " | score | completions\n"
 
         guild = client.get_guild(currentGuild)
         for player in players:
@@ -122,13 +153,22 @@ async def print_leaderboard(message, key):
             name = guild.get_member(int(player_id))
             if name is None:
                 continue
+
+            real_name = emoji.demojize(name.display_name)
+            if (len(real_name) > 12):
+                list1 = list(real_name)
+                list1[9] = '.'
+                list1[10] = '.'
+                list1[11] = '.'
+                real_name = ''.join(list1)
+
             # name = await client.fetch_user(int(player_id))
             name_formatter = '{message:{fill}{align}{width}}'.format(
-               # message=name.nick,
-               message=name.display_name,
-               fill=' ',
-               align='<',
-               width=12,
+                # message=name.nick,
+                message=real_name[:12],
+                fill=' ',
+                align='<',
+                width=12,
             )
             score_formatter = '{message:{fill}{align}{width}}'.format(
                 message=str(player[key]),
@@ -136,7 +176,8 @@ async def print_leaderboard(message, key):
                 align='<',
                 width=5,
             )
-            leaderboard += str(i) + ". " + name_formatter + " | " + score_formatter + " | " + str(data[str(name.id)]["completions"]) + "\n"
+            leaderboard += str(i) + ". " + name_formatter + " | " + score_formatter + " | " + str(
+                data[str(name.id)]["completions"]) + "\n"
             i += 1
         leaderboard += "```"
         await channel.send(leaderboard)
@@ -144,69 +185,38 @@ async def print_leaderboard(message, key):
 
 def create_user(id):
     new_user = {str(id):
-                    {
-                        "total_score": 0,
-                        "playedToday": False,
-                        "completions": 0,
-                        "weekly_score": 0,
-                        "recent_scores": [0, 0, 0, 0, 0, 0, 0]
-                    }
-                }
+        {
+            "total_score": 0,
+            "playedToday": False,
+            "completions": 0,
+            "weekly_score": 0
+        }
+    }
     return new_user
 
 
-async def inject_score(user_list, user, score):
+async def inject_score(data, user, score):
     # increment user score
     score = 7 - score
-    user_list[user]["total_score"] += score
-    user_list[user]["weekly_score"] = user_list[user]["weekly_score"] + score - user_list[user]["recent_scores"][6]
-
-    # move other scores back
-    for i in range(6, 0, -1):
-        user_list[user]["recent_scores"][i] = user_list[user]["recent_scores"][i - 1]
-    user_list[user]["recent_scores"][0] = score
-
-    top_score = 0
-    top_wordler = ""
-
-    for player in user_list:
-        if user_list[player]["weekly_score"] > top_score:
-            top_score = user_list[player]["weekly_score"]
-            top_wordler = player
+    data[user]["total_score"] += score
+    data[user]["weekly_score"] += score
 
     guild = client.get_guild(currentGuild)
     role = get(guild.roles, name='Top Wordler')
-    winner = guild.get_member(int(top_wordler))
-    if len(role.members) > 0:
-        await role.members[0].remove_roles(role)
-    await winner.add_roles(role)
 
-    # update top wordler
-    # key = "weekly_score"
-    # players = []
-    # top_score = 0
-    # top_player = None
-    # for player in user_list:
-    #     players.append({'id': player, key: user_list[player][key]})
-    #
-    # for player in players:
-    #     score = player[key]
-    #     if score > top_score:
-    #         top_score = score
-    #         top_player = player['id']
-    #
-    # guild = client.get_guild(currentGuild)
-    # role = get(guild.roles, name='Top Wordler')
-    # winner = guild.get_member(int(top_player))
-    # print(winner)
-    # print(top_player)
-    #
-    # if len(role.members) > 0:
-    #     await role.members[0].remove_roles(role)
-    # if winner != None:
-    #     await winner.add_roles(role)
+    best_score = 0
+    top_wordler = None
 
-    return user_list
+    for player in data:
+        if data[player]['weekly_score'] > best_score:
+            top_wordler = player
+            best_score = data[player]['weekly_score']
+
+    for player in role.members:
+        await player.remove_roles(role)
+    await guild.get_member(int(top_wordler)).add_roles(role)
+
+    return data
 
 
 async def handle_submission(message):
@@ -240,6 +250,7 @@ async def handle_submission(message):
                                str(message.content[12]) + guess + ". " +
                                str(client.get_guild(currentGuild).get_member(int(message.author.id)).display_name) +
                                "'s total: " + str(data[str(message.author.id)]["total_score"]))
+            await send_snark(int(message.content[12]), channel)
 
         data[str(message.author.id)]["playedToday"] = True
 
@@ -247,27 +258,62 @@ async def handle_submission(message):
         f.write(json.dumps(data, indent=4))
 
 
+async def send_snark(guesses, channel):
+    # messages = [
+    #     ["fuck you"],
+    #     ["kinda sus"],
+    #     ["thank you Sydney, very cool", "impressive!", "you must feel pretty good right about now",
+    #      "stop you're making everyone jealous", "poggers",
+    #      "this has been the highlight of my day. I have a boring life"],
+    #     ["birdie", "wow you did slightly better than average", "nice job", "three is like one less than four, crazy"],
+    #     ["Wow that's so average", "holy shit you did fine", "kinda expected, coming from you", "meh.",
+    #      "my job is so dull sometimes"],
+    #     ["rough day", "oooh that's tough", "at least it's not 6 :)", "wooooooooah, you suck", "you can do better",
+    #                                                                     "I guess somebody had to suck today"],
+    #     ["by the skin of your teeth!", "at least you didn't lose :)", "le mao",
+    #      "wow that's the worst I've seen all day",
+    #      "you must be joking", "L + ratio", ":copium::copium::copium:"]
+    # ]
+    with open('secrets/quips.json', 'r') as f:
+        data = json.load(f)
+        messages = data
+
+    await channel.send(messages[str(guesses)][random.randint(0, len(messages[str(guesses)]) - 1)])
+
+
 def daily_reset():
     threading.Timer(1, daily_reset).start()
     tz = pytz.timezone('US/Eastern')
     now = datetime.now(tz)
     current_time = now.strftime("%H:%M:%S")
-    # print("Current Time =", current_time)
     if current_time == '00:00:00':
         # print("test")
         print("working")
+        channel = client.get_channel(responseChannel)
+        # await channel.send("resetting the day")
         with open('secrets/scores.json', 'r') as f:
             data = json.load(f)
             for player in data:
                 data[player]['playedToday'] = False
+
+            weekly_reset(data)
+
         with open('secrets/scores.json', 'w') as f:
             f.write(json.dumps(data, indent=4))
 
 
+def weekly_reset(data=None):
+    if data is None:
+        return
+    if datetime.today().isoweekday() == 7:
+        for player in data:
+            data[player]['weekly_score'] = 0
+
+
 daily_reset()
+weekly_reset()
 
 # wordlebot toeken
 with open('secrets/token.json', 'r') as f:
     token = str(json.load(f)['token'])
     client.run(token)
-
